8550e9bf207abfb5686faf647199842177431320
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-13 07:29:20 +0200

    Let and Const

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,11 +1,11 @@
-var name = 'Jack';
-console.log(name);
-name = 'Jane';
-console.log(name);
-var color;
-console.log(color);
-color = 'red';
-console.log(color);
+// let name = 'Jack';
+// console.log(name);
+// name = 'Jane';
+// console.log(name);
+// let color;
+// console.log(color);
+// color = 'red';
+// console.log(color);
 
 
 /* JavaScript variables names can include
@@ -15,10 +15,42 @@ JavaScript variables names can't start with number
 */
 
 
-$color = 'green';
-_color = 'orange';
+// $color = 'green';
+// _color = 'orange';
 
-myMainColor = 'black' //camel case
-my_main_color // snake case
-my-main-color // kebab case
-MyMainCase // Pascal case
+// myMainColor = 'black' //camel case
+// my_main_color // snake case
+// my-main-color // kebab case
+// MyMainCase // Pascal case
+
+
+const name = 'Jack';
+console.log(name);
+// name = 'Kail';
+
+const color =  'red';
+console.log(color);
+
+const user = {
+    name: 'Jack',
+    gender: 'male',
+    age: 24,
+}
+
+console.log(user);
+
+user.name = 'Jane';
+
+console.log(user);
+
+// user = {
+//     name: 'Jane', // Wrong
+//     gender: 'male',
+//     age: 24,
+// }
+
+const colors = ['red', 'yellow', 'orange', 'green']
+console.log(colors)
+
+colors.push('blue')
+console.log(colors)
