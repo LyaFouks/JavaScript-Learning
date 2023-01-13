aee019c3fe21592e5f012400475ad35f00e5d396
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-13 07:19:55 +0200

    Variables and comments

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,3 +1,24 @@
-console.log(
-    'Hello console!'
-)
+var name = 'Jack';
+console.log(name);
+name = 'Jane';
+console.log(name);
+var color;
+console.log(color);
+color = 'red';
+console.log(color);
+
+
+/* JavaScript variables names can include
+letters, numbers, $, _ 
+
+JavaScript variables names can't start with number
+*/
+
+
+$color = 'green';
+_color = 'orange';
+
+myMainColor = 'black' //camel case
+my_main_color // snake case
+my-main-color // kebab case
+MyMainCase // Pascal case
