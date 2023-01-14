a451c8bd82c4378d63b31d80173f52ba0115d2d0
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-14 09:31:42 +0200

    Type Conversion

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,66 +1,66 @@
-// let x = 'something';
-// console.log(x);
-//  x = 3;
-// console.log(x);
+// Conversion to string
 
-// Primitive data types
+// String()
 
-// String
+let x = String(1112);
+x = String(2+2);
+x = String(false);
+x = String(new Date());
+x = String(['one', 'two', 'three']);
 
-const someText = 'Some text';
-console.log(typeof someText);
+// toString()
 
-// Number
-
-const someNumber = 11;
-console.log(typeof someNumber);
-
-// Boolean
-
-const someBoolean = false;
-console.log(typeof someBoolean);
-
-// Null
-
-const someNull= null;
-console.log(typeof someNull);
-
-// Undefined
-
-let someUndefined;
-console.log(typeof someUndefined);
-
-// Symbol
-
-const someSymbol = Symbol();
-console.log(typeof someSymbol);
+x = (35).toString();
+x = (true).toString();
 
 
+console.log(x);
+console.log(typeof x);
+console.log(x.length);
 
-// Reference data types
 
-// Array
+// Conversion to number
 
-const someArray = [1, 2, 3, 4];
-console.log(typeof someArray);
+//Number()
 
-// Object Literal
+let y = Number('5.123147613515');
+y = Number(false);
+y = Number(null);
+y = Number('text');
+y = Number([1, 2, 3]);
 
-const someObjectLiteral = {
-    firstProperty : '12',
-    secondProperty: 'some',
 
-};
-console.log(typeof someObjectLiteral);
+// parseInt()
 
-// Function
+y = parseInt('123.23');
+y = parseFloat('123.23');
 
-const someFunction = new Function();
-console.log(typeof someFunction);
 
-// Date
+console.log(y);
+console.log(typeof y);
+console.log(y.toPrecision(4));
 
-const someDate = new Date();
-console.log(typeof someDate);
+
+const a = "5";
+const b = "3";
+const c = a + b;
+
+console.log(z);
+console.log(typeof z);
+
+const d = "5";
+const s = "3";
+const e = d - s;
+
+console.log(e);
+console.log(typeof e);
+
+const q = "5";
+const w = "3";
+const r = q * w;
+
+console.log(r);
+console.log(typeof r);
+
 
 
