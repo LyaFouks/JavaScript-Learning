676e70a01598a7ec12710454588257cc65124b93
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-15 09:16:14 +0200

    Templatee litterals or temlate strings

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,90 +1,42 @@
-let result;
-const firstName = 'Jack';
-const lastName = 'Brown';
-const space = ' ';
-const age = 25;
-const greeting = 'Hey there!';
+const brand = 'Toyota';
+const model = 'Camry';
+const color = 'Gray';
+const year = '1999';
 
-
-// Concatanation
-
-result = firstName + space + lastName;
-
-result = greeting + space + 'My name is' + space + firstName + ' ' + lastName + ". I'am " + age + ' years old.';
-
-
-// Escaping
-
-result = 'I\'m 34 years old';
-result = "I like \"Star wars\" movie.";
-
-
-// Properties and methods
-
-result = 'Hello!'.length;
-result = firstName.length;
-
-
-// Concat
-
-result = firstName.concat(' ', lastName);
-result = greeting.concat(space, 'My name is ', firstName, space, lastName);
-
-
-// toUpperCase() / toLowerCase()
-
-result = result.toUpperCase();
-result = result.toLowerCase();
-
-
-// indexOf()
-
-result = firstName[2];
-
-result = 'Hello'.indexOf('l');
-result = 'Hello lol'.lastIndexOf('l');
-result = firstName.indexOf('a');
-result = 'Hello lol'.lastIndexOf('lo');
-result = firstName.indexOf('z');
-
-
-// charAt()
-
-result = firstName.charAt(1);
-
-const longString = 'Hi, I\'am a long string';
-
-result = longString.charAt(longString.length -1);
-
-
-// substring()
-
-result = greeting.substring(4, 9);
-
-
-// slice()
-
-result = greeting.slice(4, 9);
-result = greeting.slice(-5);
-
-
-// split()
-
-result = longString.split(' ');
-const colors = 'red, orange, green, blue,';
-result = colors.split(',');
-
-
-// replace()
-
-result = colors.replace('blue', 'yellow');
-
-
-// includes()
-
-result = colors.includes('blue');
+let carHtml;
 
 
 
+// Old approach (before ES6)
 
-console.log(result);
+carHtml = '<h3>' + 'Car Description' + '</h3>' +
+'<ul>' + 
+'<li>Brand: ' + brand + '</li>' +
+'<li>Model: ' + model + '</li>' +
+'<li>Color: ' + color + '</li>' +
+'<li>Year: ' + year + '</li>' +
+'</ul>';
+
+document.body.innerHTML = carHtml;
+
+
+// New approach (Template litterals or template strings)
+
+carHtml = `
+<h3>Car Description</h3>
+<ul>
+<li>Brand: ${brand}</li>
+<li>Model: ${model}</li>
+<li>Color:  ${color}</li>
+<li>Year: ${year}</li>
+<li>Doors: ${getDoorsNumber()}</li>
+<li>Tax: ${year < 2000 ? '20%' : '10%'}</li>
+</ul>;
+`
+
+
+function getDoorsNumber() {
+    return 5;
+}
+
+document.body.innerHTML = carHtml;
