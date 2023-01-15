5d09d4312d23f50f2cff26af81d8b5ab47787b81
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-15 08:31:27 +0200

    Operations with Numbers

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,66 +1,32 @@
-// Conversion to string
-
-// String()
-
-let x = String(1112);
-x = String(2+2);
-x = String(false);
-x = String(new Date());
-x = String(['one', 'two', 'three']);
-
-// toString()
-
-x = (35).toString();
-x = (true).toString();
+const firstNumber = 50;
+const secondNumber = 10;
+let = result;
 
 
-console.log(x);
-console.log(typeof x);
-console.log(x.length);
+// Simple math operation
+
+result = firstNumber + secondNumber;
+result = firstNumber - secondNumber;
+result = firstNumber * secondNumber;
+result = firstNumber / secondNumber;
+result = firstNumber % secondNumber;
 
 
-// Conversion to number
+// Math object
 
-//Number()
-
-let y = Number('5.123147613515');
-y = Number(false);
-y = Number(null);
-y = Number('text');
-y = Number([1, 2, 3]);
-
-
-// parseInt()
-
-y = parseInt('123.23');
-y = parseFloat('123.23');
-
-
-console.log(y);
-console.log(typeof y);
-console.log(y.toPrecision(4));
-
-
-const a = "5";
-const b = "3";
-const c = a + b;
-
-console.log(c);
-console.log(typeof c);
-
-const d = "5";
-const s = "3";
-const e = d - s;
-
-console.log(e);
-console.log(typeof e);
-
-const q = "5";
-const w = "3";
-const r = q * w;
-
-console.log(r);
-console.log(typeof r);
+result = Math.PI;
+result = Math.round(3.4);
+result = Math.ceil(3.3);
+result = Math.floor(3.8);
+result = Math.sqrt(9);
+result = Math.abs(-7);
+result = Math.pow(2, 3);
+result = Math.min(3, 45, 22, 151, 11, -67);
+result = Math.max(3, 45, 22, 151, 11, -67);
+result = Math.random();
+result = Math.floor(Math.random() * 100);
 
 
 
+
+console.log(result);
