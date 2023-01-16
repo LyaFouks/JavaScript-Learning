b567a08a1caf74b6dfecb323f650a9eeee786402
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-16 08:00:06 +0200

    Task ''Questionnaire"

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,42 +1,9 @@
-const brand = 'Toyota';
-const model = 'Camry';
-const color = 'Gray';
-const year = '1999';
+let userName = prompt('What is your first name?', '');
 
-let carHtml;
+let userLastName = prompt('What is your last name?', '');
 
+let userAge = prompt('What is your age?', '');
 
+const greeting = 'Hi there, ' + userName + ' ' + userLastName + '. ' + 'You\'re ' + userAge + ' years old.';
 
-// Old approach (before ES6)
-
-carHtml = '<h3>' + 'Car Description' + '</h3>' +
-'<ul>' + 
-'<li>Brand: ' + brand + '</li>' +
-'<li>Model: ' + model + '</li>' +
-'<li>Color: ' + color + '</li>' +
-'<li>Year: ' + year + '</li>' +
-'</ul>';
-
-document.body.innerHTML = carHtml;
-
-
-// New approach (Template litterals or template strings)
-
-carHtml = `
-<h3>Car Description</h3>
-<ul>
-<li>Brand: ${brand}</li>
-<li>Model: ${model}</li>
-<li>Color:  ${color}</li>
-<li>Year: ${year}</li>
-<li>Doors: ${getDoorsNumber()}</li>
-<li>Tax: ${year < 2000 ? '20%' : '10%'}</li>
-</ul>;
-`
-
-
-function getDoorsNumber() {
-    return 5;
-}
-
-document.body.innerHTML = carHtml;
+alert(greeting);
