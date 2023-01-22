c4e29ec1c3811abffd3c9f28e904e75ff3ba03c5
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-22 09:08:05 +0200

    Overweight Task with If Else

--- OverweightTask/script.js
+++ OverweightTask/script.js
@@ -3,10 +3,13 @@ const height = prompt('Enter your height in meters');
 
 const bodyMassIndex = weight / Math.pow(height, 2);
 
-const areYouOverweight = 
-bodyMassIndex >= 25;
 
-console.log(bodyMassIndex);
-console.log(areYouOverweight);
 
-alert('Are you overweight?' + areYouOverweight);
+if(bodyMassIndex >= 25){
+    alert('You are overweight!')
+    console.log('You are overweight!')
+} else {
+    alert('No, you have normal weight!')
+    console.log('No, you have normal weight!')
+}
+
