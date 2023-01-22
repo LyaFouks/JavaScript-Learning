0d29c8ba2f2393e7df7a5d063868b702a084f701
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-22 09:32:12 +0200

    Boolean Logical Operators

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,29 +1,46 @@
-// JavaScript falsy values:
-// 0, '', undefined, null, NaN
-// console.log(Boolean(0));
-// console.log(Boolean(''));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(NaN));
+// AND, OR, NOT
 
-console.log(Boolean(32));
-console.log(Boolean('hello'));
-console.log(Boolean({}));
-console.log(Boolean([]));
-console.log(Boolean(3.5));
+// AND &&
+// console.log(true && true); //true
+// console.log(true && false); //false
+// console.log(false && false); // false
 
-const age = 0;
+// OR ||
+// console.log(true || true); //true
+// console.log(true || false); //true
+// console.log(false || false); //false
 
-if(age) {
-   console.log('The person was born.');
+// NOT !
+// console.log(!true); //false
+// console.log(!false); //true
+
+
+// const hasMoney = true;
+// const isAdult = false;
+
+// if(hasMoney && isAdult) {
+//    console.log('You can buy the drink.');
+// } else {
+//    console.log('You can not buy the drink.');
+// }
+
+
+// const hasMoney = false;
+// const withParent = false;
+
+// if(hasMoney || withParent) {
+//    console.log('You can buy the orange.');
+// } else {
+//    console.log('You can not buy the orange.');
+// }
+
+
+const hasMoney = true;
+const isAdult = true;
+const isCompletelyDrunk = true;
+
+if(hasMoney && isAdult && !isCompletelyDrunk) {
+   console.log('You can buy the drink.');
 } else {
-   console.log('The person was not born yet.');
-}
-
-let weight;
-
-if(weight) {
-   console.log('Weight is defined.');
-} else {
-   console.log('Weight is not defined.');
+   console.log('You can not buy the drink.');
 }
