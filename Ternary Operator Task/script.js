ce1ead0c57ccfb6cd2bf1464935d08477c69cbbc
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-24 18:23:22 +0200

    Ternary operator task

---
+++ Ternary Operator Task/script.js
@@ -0,0 +1,7 @@
+const userAge = prompt("Enter your age", 0);
+const adult = 'You can buy the alcohol.';
+const teenager ="You are too young.";
+
+(userAge >= 18) ? 
+ alert(adult) + console.log(adult) :
+ alert(teenager) + console.log(teenager); 
