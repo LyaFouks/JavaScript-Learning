6b70a16eea8704583d0d13438a29359a4df4bc16
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-25 07:44:30 +0200

    Array Methods

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,37 +1,57 @@
-const color1 = 'red';
-const color2 = 'orange';
-const color3 = 'yellow';
-const color4 = 'green';
-const color5 = 'blue';
-const color6 = 'indigo';
-const color7 = 'violet';
-console.log(color2);
-
-
-const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-rainbowColors[5] = 'dark blue';
-rainbowColors[rainbowColors.length] = 'indigo';
-console.log(rainbowColors);
-console.log(`The sky is ` + rainbowColors[4]);
-
-
-const stringNumbers = [];
-stringNumbers[0] = 'one';
-stringNumbers[1] = 'two';
-stringNumbers[2] = 'three';
-console.log(stringNumbers);
-
-
-const stringNumbers2 = new Array('one', 'two', 'three');
-console.log(stringNumbers2);
-
-
-
 const numbers = [32, 2, 65, 123];
+let result;
+
+result = numbers.length;
+console.log(result);
+
+result = Array.isArray(numbers);
+console.log(result);
+
+result = numbers.indexOf(65);
+console.log(result);
+
+// push(), pop()
+
+result = numbers.push(87);
+console.log(result);
+
+result = numbers.push(63, 32, 15);
+console.log(result);
+
+result = numbers.pop();
+console.log(result);
+
+// shift(), unshift()
+
+result = numbers.unshift(3);
+console.log(result);
+
+result = numbers.unshift(98, 132, 56);
+
+result = numbers.shift();
+console.log(result);
+
+
+
+result = numbers.splice(1, 1);
+console.log(result);
+result = numbers.splice(2, 3);
+console.log(result);
+
+
+
+result = numbers.reverse();
+console.log(result);
+
+
+
+result = numbers.slice(1, 4);
+console.log(result);
+
+
+
+result = numbers.concat(result);
+console.log(result);
+
+
 console.log(numbers);
-
-
-
-const anyItems = [4, true, new Date(), 'something'];
-console.log(anyItems);
