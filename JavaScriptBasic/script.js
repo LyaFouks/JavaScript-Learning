8f96611e7b0734c056861e3c2ea4afa131a9964a
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-25 07:31:01 +0200

    Arrays

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,17 +1,37 @@
-function colorMixer(color1, color2) {
-   const newColor = `${color1} + ${color2}`;
-   return newColor;
-}
+const color1 = 'red';
+const color2 = 'orange';
+const color3 = 'yellow';
+const color4 = 'green';
+const color5 = 'blue';
+const color6 = 'indigo';
+const color7 = 'violet';
+console.log(color2);
+
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+rainbowColors[5] = 'dark blue';
+rainbowColors[rainbowColors.length] = 'indigo';
+console.log(rainbowColors);
+console.log(`The sky is ` + rainbowColors[4]);
+
+
+const stringNumbers = [];
+stringNumbers[0] = 'one';
+stringNumbers[1] = 'two';
+stringNumbers[2] = 'three';
+console.log(stringNumbers);
+
+
+const stringNumbers2 = new Array('one', 'two', 'three');
+console.log(stringNumbers2);
 
 
 
-function colorizer(item, color1, color2) {
-   const color = colorMixer(color1, color2);
-    const colorizedItem = `The ${item} is ${color}.`;
-     return colorizedItem;
-}
+const numbers = [32, 2, 65, 123];
+console.log(numbers);
 
-const colorizedCar = colorizer('car', 'red', 'orange');
-console.log(colorizedCar);
 
-console.log(colorizer('house', 'white', 'black'));
+
+const anyItems = [4, true, new Date(), 'something'];
+console.log(anyItems);
