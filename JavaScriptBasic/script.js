724e55186bf6c9d177245817f24ce976a231c676
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-26 13:31:52 +0200

    Objects

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,57 +1,7 @@
-const numbers = [32, 2, 65, 123];
-let result;
-
-result = numbers.length;
-console.log(result);
-
-result = Array.isArray(numbers);
-console.log(result);
-
-result = numbers.indexOf(65);
-console.log(result);
-
-// push(), pop()
-
-result = numbers.push(87);
-console.log(result);
-
-result = numbers.push(63, 32, 15);
-console.log(result);
-
-result = numbers.pop();
-console.log(result);
-
-// shift(), unshift()
-
-result = numbers.unshift(3);
-console.log(result);
-
-result = numbers.unshift(98, 132, 56);
-
-result = numbers.shift();
-console.log(result);
-
-
-
-result = numbers.splice(1, 1);
-console.log(result);
-result = numbers.splice(2, 3);
-console.log(result);
-
-
-
-result = numbers.reverse();
-console.log(result);
-
-
-
-result = numbers.slice(1, 4);
-console.log(result);
-
-
-
-result = numbers.concat(result);
-console.log(result);
-
-
-console.log(numbers);
+const myData = {
+    name: 'Nikita',
+    lastName: 'Lya',
+    birthYear: 1991,
+    proffesion: 'Developer',
+    familyMembers: ['Anjela', 'Zabava'],
+};
