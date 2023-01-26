3d3657573a717825f54e88738929e032c1a5d089
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-26 17:31:35 +0200

    Objects.Dot vs Bracket Notation

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -3,5 +3,36 @@ const myData = {
     lastName: 'Lya',
     birthYear: 1991,
     proffesion: 'Developer',
-    familyMembers: ['Anjela', 'Zabava'],
+    familyMembers: ['Anjela', 'Zabava', 'Nikita'],
 };
+
+
+console.log(myData);
+console.log(myData.name);
+console.log(myData['name']);
+
+const baseName = ' name';
+console.log(myData[`lastName`] + baseName);
+
+// const userInput = prompt('What do you want to know about Nikita? Choose between name, lastName, birthYear, proffesion, familyMembers');
+
+// if(myData[userInput]) {
+//     console.log(myData[userInput]);
+// } else {
+//     console.log('This property does not exist. Choose between name, lastName, birthYear, proffesion, familyMembers')
+// }
+
+
+// Create new properties
+
+myData.location = 'Ukraine';
+myData['Skype'] = 'itssantinno';
+
+console.log(myData);
+
+
+// Challenge
+
+const message = `${myData.name} has ${myData.familyMembers.length} family members and the first one is ${myData.familyMembers[0]}`
+
+console.log(message);
