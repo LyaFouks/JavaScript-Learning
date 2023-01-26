c6ba197ee28c414c43e89915bf5d051e05614355
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-26 20:33:38 +0200

    Objects Methods

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -4,35 +4,35 @@ const myData = {
     birthYear: 1991,
     proffesion: 'Developer',
     familyMembers: ['Anjela', 'Zabava', 'Nikita'],
+    hasDriverLicense: false,
+    // calcAge: function(birthYear) {
+    //     return 2023 - birthYear;
+    // }
+    // calcAge: function() {
+    //     // console.log(this);
+    //     return 2023 - this.birthYear;
+    // }
+    calcAge: function() {
+        return this.age = 2023 - this.birthYear;
+        
+    },
+
+    getSummary: function() {
+        return `${this.name} is a ${this.calcAge()}-years old ${this.proffesion} and he ${this.hasDriverLicense ? "has" : "hasn't"} a driver license`;
+    }
 };
 
 
-console.log(myData);
-console.log(myData.name);
-console.log(myData['name']);
 
-const baseName = ' name';
-console.log(myData[`lastName`] + baseName);
+// console.log(myData.calcAge());
+// console.log(myData.calcAge());
+// console.log(myData.calcAge());
+// console.log(myData.calcAge());
+// console.log(myData.age);
 
-// const userInput = prompt('What do you want to know about Nikita? Choose between name, lastName, birthYear, proffesion, familyMembers');
-
-// if(myData[userInput]) {
-//     console.log(myData[userInput]);
-// } else {
-//     console.log('This property does not exist. Choose between name, lastName, birthYear, proffesion, familyMembers')
-// }
+console.log(myData.getSummary());
 
 
-// Create new properties
 
-myData.location = 'Ukraine';
-myData['Skype'] = 'itssantinno';
+// Chalenge
 
-console.log(myData);
-
-
-// Challenge
-
-const message = `${myData.name} has ${myData.familyMembers.length} family members and the first one is ${myData.familyMembers[0]}`
-
-console.log(message);
