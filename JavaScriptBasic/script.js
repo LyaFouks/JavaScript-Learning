10d15bc1b5c20a415cd7b9a8fac4ad665e8b4250
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-27 08:01:52 +0200

    Cycles and Methods

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -1,3 +1,27 @@
-for(let rep = 1; rep <= 10; rep = rep + 2) {
-    console.log(`Push ups repetition ${rep}`);
+// for(let rep = 1; rep <= 10; rep = rep + 2) {
+//     console.log(`Push ups repetition ${rep}`);
+// }
+
+// const userOne = ['Nikita', 'Lya', 1991, 'Developer', ['Anjela', 'Zabava']];
+
+// const types = [];
+
+// for(let i = 0; i < userOne.length; i++) {
+//     console.log(userOne[i], typeof userOne[i]);
+
+//     // types[i] = typeof userOne[i];
+// //    types.push(typeof userOne[i]);
+//    types.unshift(typeof userOne[i]);
+// }
+
+// console.log(types);
+
+
+const birthYears = [1994, 1991, 1965, 2011];
+const ages = [];
+
+for(let i = 0; i < birthYears.length; i++) { 
+    ages.push(2023 - birthYears[i]);
 }
+
+console.log(ages);
