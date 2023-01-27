c4d64e31cd8e2a9d339d16465b410594ab34bcb5
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-27 16:37:31 +0200

    Break and Continue

--- JavaScriptBasic/script.js
+++ JavaScriptBasic/script.js
@@ -17,11 +17,33 @@
 // console.log(types);
 
 
-const birthYears = [1994, 1991, 1965, 2011];
-const ages = [];
+// const birthYears = [1994, 1991, 1965, 2011];
+// const ages = [];
 
-for(let i = 0; i < birthYears.length; i++) { 
-    ages.push(2023 - birthYears[i]);
+// for(let i = 0; i < birthYears.length; i++) { 
+//     ages.push(2023 - birthYears[i]);
+// }
+
+// console.log(ages);
+
+
+// Continue and break
+
+const userOne = ['Nikita', 'Lya', 1991, 'Developer', ['Anjela', 'Zabava'], false];
+
+
+console.log('CONTINEU EXAMPLE!');
+
+for(let i = 0; i < userOne.length; i++) {
+if(typeof userOne[i] !== 'string' ) continue;
+    console.log(userOne[i], typeof userOne[i]);
 }
 
-console.log(ages);
+
+console.log('BREAK EXAMPLE!');
+
+for(let i = 0; i < userOne.length; i++) {
+    if(typeof userOne[i] === 'number' ) break;
+        console.log(userOne[i], typeof userOne[i]);
+    }
+
