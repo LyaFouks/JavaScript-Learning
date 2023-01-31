a1b973e958165a1894cb360ffbcac755c85efee3
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-31 20:17:51 +0200

    Some DOM manipulation

--- DOM manipulation/Project 1 Guess the number/script.js
+++ DOM manipulation/Project 1 Guess the number/script.js
@@ -1,3 +1,13 @@
 "use strict";
 
 console.log(document.querySelector(".guess-message").textContent);
+
+document.querySelector(".guess-message").textContent = "Правильно";
+
+document.querySelector(".question").textContent = 7;
+
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".number-input").value);
+
+document.querySelector(".number-input").value = 13;
