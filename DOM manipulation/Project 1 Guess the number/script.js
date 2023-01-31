17dd2303391f24c05c9c12590db8911ebb786f7c
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-01-31 20:38:45 +0200

    Events & Event Handlers

--- DOM manipulation/Project 1 Guess the number/script.js
+++ DOM manipulation/Project 1 Guess the number/script.js
@@ -1,13 +1,23 @@
 "use strict";
 
-console.log(document.querySelector(".guess-message").textContent);
+// console.log(document.querySelector(".guess-message").textContent);
 
-document.querySelector(".guess-message").textContent = "Правильно";
+// document.querySelector(".guess-message").textContent = "Правильно";
 
-document.querySelector(".question").textContent = 7;
+// document.querySelector(".question").textContent = 7;
 
-document.querySelector(".score").textContent = 10;
+// document.querySelector(".score").textContent = 10;
 
-console.log(document.querySelector(".number-input").value);
+// console.log(document.querySelector(".number-input").value);
 
-document.querySelector(".number-input").value = 13;
+// document.querySelector(".number-input").value = 13;
+
+document.querySelector(".check").addEventListener("click", function () {
+	const guessingNumber = Number(document.querySelector(".number-input").value);
+
+	console.log(guessingNumber);
+
+	if (!guessingNumber) {
+		document.querySelector(".guess-message").textContent = "Введите число!";
+	}
+});
