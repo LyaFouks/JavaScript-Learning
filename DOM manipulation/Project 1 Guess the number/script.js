207fe9d2719ca8fdd9252db2bb2971a5725b9135
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-02 08:02:13 +0200

    Code refactoring

--- DOM manipulation/Project 1 Guess the number/script.js
+++ DOM manipulation/Project 1 Guess the number/script.js
@@ -1,20 +1,20 @@
 "use strict";
 
-// console.log(document.querySelector(".guess-message").textContent);
-
-// document.querySelector(".guess-message").textContent = "Правильно";
-
-// document.querySelector(".question").textContent = 7;
-
-// document.querySelector(".score").textContent = 10;
-
-// console.log(document.querySelector(".number-input").value);
-
-// document.querySelector(".number-input").value = 13;
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+
+const displayGuessMessage = function (message) {
+	document.querySelector(".guess-message").textContent = message;
+};
+
+const displayScore = function (score) {
+	document.querySelector(".score").textContent = score;
+};
+
+const bodyBackgroundColor = function (backgroundColor) {
+	document.querySelector("body").style.backgroundColor = backgroundColor;
+};
 
 // Logic implementation
 
@@ -25,54 +25,45 @@ document.querySelector(".check").addEventListener("click", function () {
 
 	// No input
 	if (!guessingNumber) {
-		document.querySelector(".guess-message").textContent = "Введите число!";
+		displayGuessMessage("Введите Число!");
 
 		// Player won
 	} else if (guessingNumber === secretNumber) {
-		document.querySelector(".guess-message").textContent = "Правильно!";
+		displayGuessMessage("Правильно!");
 		document.querySelector(".question").textContent = secretNumber;
-		document.querySelector("body").style.backgroundColor = "rgb(9, 250, 21)";
+		bodyBackgroundColor("rgb(9, 250, 21)");
 		document.querySelector(".question").style.width = "50rem";
+
+		// The best result
 		if (score > highScore) {
 			highScore = score;
 		}
 		document.querySelector(".highscore").textContent = highScore;
 
-		// To high
-	} else if (guessingNumber > secretNumber) {
+		// Number from input is wrong
+	} else if (guessingNumber !== secretNumber) {
 		if (score > 1) {
-			document.querySelector(".guess-message").textContent = "Слишком много!";
+			displayGuessMessage(
+				guessingNumber > secretNumber ? "Слишком много!" : "Слишком мало!"
+			);
 			score--;
-			document.querySelector(".score").textContent = score;
+			displayScore(score);
 		} else {
-			document.querySelector(".guess-message").textContent = "Game Over!";
-			document.querySelector(".score").textContent = 0;
-		}
-
-		// To low
-	} else if (guessingNumber < secretNumber) {
-		if (score > 1) {
-			document.querySelector(".guess-message").textContent = "Слишком мало!";
-			score--;
-			document.querySelector(".score").textContent = score;
-		} else {
-			document.querySelector(".guess-message").textContent = "Game Over!";
-			document.querySelector(".score").textContent = 0;
+			displayGuessMessage("Game Over!");
+			displayScore(0);
 		}
 	}
 });
 
-// Button "Again" function
+// Button "Again" functional
 
 document.querySelector(".again").addEventListener("click", function () {
 	secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 	document.querySelector(".question").textContent = "???";
 	document.querySelector(".question").style.width = "25rem";
-	document.querySelector(".guess-message").textContent = "Начни угадывать!";
-	document.querySelector(".score").textContent = score = 20;
+	displayGuessMessage("Начни угадывать!");
+	displayScore((score = 20));
 	document.querySelector(".number-input").value = "";
-	document.querySelector("body").style.backgroundColor = "rgb(0, 0, 0)";
+	bodyBackgroundColor("rgb(0, 0, 0)");
 });
-
-// The best result
