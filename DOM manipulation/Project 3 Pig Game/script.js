0abb9161c8e2892158e40941a70cd8582f16e5fa
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-04 11:41:49 +0200

    Project 3 Pig Game

---
+++ DOM manipulation/Project 3 Pig Game/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Element selections
+
+const score0Element = document.querySelector("#score--0");
+const score1Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+// Game initital conditions
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add("hidden");
