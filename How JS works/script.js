c349c26d1b2ac17cc2bb1360fa32b9cb176888b9
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-07 16:06:21 +0200

    This keyword

--- How JS works/script.js
+++ How JS works/script.js
@@ -3,48 +3,87 @@
 // Hoisting
 
 // Variables
-console.log(firstname);
-// console.log(job);
-// console.log(birthYear);
+// console.log(firstname);
+// // console.log(job);
+// // console.log(birthYear);
 
-var firstname = "Nikita";
-let job = "developer";
-const birthYear = 1991;
+// var firstname = "Nikita";
+// let job = "developer";
+// const birthYear = 1991;
 
-// Functions
+// // Functions
 
-console.log(decSum(1, 2));
-// console.log(exprSum(1, 2));
-// console.log(arrSum(1, 2));
+// console.log(decSum(1, 2));
+// // console.log(exprSum(1, 2));
+// // console.log(arrSum(1, 2));
 
-function decSum(a, b) {
-	return a + b;
-}
+// function decSum(a, b) {
+// 	return a + b;
+// }
 
-const exprSum = function (a, b) {
-	return a + b;
+// const exprSum = function (a, b) {
+// 	return a + b;
+// };
+
+// var arrSum = (a, b) => a + b;
+
+// // Example
+
+// console.log(isUserValid);
+
+// if (!isUserValid) {
+// 	deleteUser();
+// }
+
+// var isUserValid = true;
+
+// function deleteUser() {
+// 	console.log("User is deleted");
+// }
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// console.log(a === window.a);
+// console.log(b === window.b);
+// console.log(c === window.c);
+
+/*******************************************************************/
+// this keyword
+
+// console.log(this);
+
+const getAge = function (birthYear) {
+	console.log(2023 - birthYear);
+	console.log(this);
 };
 
-var arrSum = (a, b) => a + b;
+getAge(1991);
 
-// Example
+const getAgeArr = (birthYear) => {
+	console.log(2023 - birthYear);
+	console.log(this);
+};
 
-console.log(isUserValid);
+getAgeArr(1991);
 
-if (!isUserValid) {
-	deleteUser();
-}
+const user187 = {
+	birthYear: 1991,
+	getAge: function () {
+		console.log(this);
+		console.log(2023 - this.birthYear);
+	},
+};
 
-var isUserValid = true;
+user187.getAge();
 
-function deleteUser() {
-	console.log("User is deleted");
-}
+const user188 = {
+	birthYear: 1993,
+};
 
-var a = 1;
-let b = 2;
-const c = 3;
+user188.getAge = user187.getAge;
+user188.getAge();
 
-console.log(a === window.a);
-console.log(b === window.b);
-console.log(c === window.c);
+const f = user187.getAge;
+// f();
