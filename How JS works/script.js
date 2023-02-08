05a60718bfb130296882999d68b59b4a62859ed7
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-08 08:42:22 +0200

    Primitives VS Reference Types

--- How JS works/script.js
+++ How JS works/script.js
@@ -90,57 +90,114 @@
 
 /******************************************************************/
 
-// Regular VS Arrow Functions
+// // Regular VS Arrow Functions
 
-// var firsName = "Jack";
+// // var firsName = "Jack";
 
-const user187 = {
-	firstName: "Nikita",
-	birthYear: 1991,
-	getAge: function () {
-		console.log(this);
-		console.log(2023 - this.birthYear);
+// const user187 = {
+// 	firstName: "Nikita",
+// 	birthYear: 1991,
+// 	getAge: function () {
+// 		console.log(this);
+// 		console.log(2023 - this.birthYear);
 
-		// Old approach
-		// const self = this; // self or that
-		// const isAdult = function () {
-		// 	console.log(self);
-		// 	// console.log(2023 - this.birthYear >= 18);
-		// 	console.log(2023 - self.birthYear >= 18);
-		// };
+// 		// Old approach
+// 		// const self = this; // self or that
+// 		// const isAdult = function () {
+// 		// 	console.log(self);
+// 		// 	// console.log(2023 - this.birthYear >= 18);
+// 		// 	console.log(2023 - self.birthYear >= 18);
+// 		// };
 
-		// New approach after ES6
-		const isAdult = () => {
-			console.log(this);
-			console.log(2023 - this.birthYear >= 18);
-		};
+// 		// New approach after ES6
+// 		const isAdult = () => {
+// 			console.log(this);
+// 			console.log(2023 - this.birthYear >= 18);
+// 		};
 
-		isAdult();
-	},
-	sayGreeting: function () {
-		// console.log(this);
-		console.log(`Hello, I'm ${this.firsName}`);
-	},
+// 		isAdult();
+// 	},
+// 	sayGreeting: function () {
+// 		// console.log(this);
+// 		console.log(`Hello, I'm ${this.firsName}`);
+// 	},
+// };
+
+// user187.sayGreeting();
+// user187.getAge();
+// // console.log(this.firsName);
+
+// /*********************************************************************/
+
+// // Arguments keyword
+// const exprSum = function (a, b) {
+// 	console.log(arguments);
+// 	return a + b;
+// };
+
+// exprSum(1, 2);
+// exprSum(1, 2, 3, 4, 6, 8, 51);
+
+// var arrSum = (a, b) => {
+// 	console.log(arguments);
+// 	return a + b;
+// };
+
+// arrSum(7, 19, 43);
+
+/**********************************************************************************************************************************************/
+
+// // Primitives VS Reference Types
+
+// let a = 1;
+// let b = 1;
+// a = 2;
+// console.log(a);
+// console.log(b);
+
+// const x = {
+// 	foo: "bar",
+// 	a: 1,
+// };
+
+// const y = x;
+// y.a = 2;
+
+// console.log("Object x: ", x);
+// console.log("Object y: ", y);
+
+// Primitives
+let lastName = "Brown";
+let oldLastName = lastName;
+lastName = "White";
+console.log(lastName, oldLastName);
+
+// Objects
+const jane = {
+	firstName: "Jane",
+	lastName: "Brown",
+	age: 21,
 };
 
-user187.sayGreeting();
-user187.getAge();
-// console.log(this.firsName);
+const marriedJane = jane;
+marriedJane.lastName = "White";
+console.log("Jane: ", jane);
+console.log("Married Jane: ", marriedJane);
 
-/*********************************************************************/
+// marriedJane = {};
 
-// Arguments keyword
-const exprSum = function (a, b) {
-	console.log(arguments);
-	return a + b;
+// Objects copying
+
+const sara = {
+	firstName: "Jane",
+	lastName: "Brown",
+	age: 21,
+	familyMemebres: ["Jack", "Bob", "Greta"],
 };
 
-exprSum(1, 2);
-exprSum(1, 2, 3, 4, 6, 8, 51);
+const saraCopy = Object.assign({}, sara);
+saraCopy.lastName = "White";
+saraCopy.familyMemebres.push("Bill", "Marta");
 
-var arrSum = (a, b) => {
-	console.log(arguments);
-	return a + b;
-};
-
-arrSum(7, 19, 43);
+console.log("Sara: ", sara);
+console.log("Married Sara: ", saraCopy);
