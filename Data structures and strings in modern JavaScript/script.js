35d47d927bb4ab0961653b6f1c04e0b35bc3fb73
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-10 21:47:25 +0200

    Spread operator

--- Data structures and strings in modern JavaScript/script.js
+++ Data structures and strings in modern JavaScript/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const japaneseRestaurant = {
-	resName: "Banzai",
+	name: "Banzai",
 	location: "108 Markham Woods Rd, Longwood, USA",
 	categories: ["Japanese", "Sushi", "Vegetarian", "Organic"],
 	appetizers: ["Seaweed salad", "Tempura shrimp", "Edamame", "Sushi rice"],
@@ -33,22 +33,84 @@ const japaneseRestaurant = {
 			`Your order on the way to you! ${this.appetizers[appetizersIndex]} and ${this.mainMenu[mainMenuIndex]} will be arrived to ${adress} at ${deliveryTime}`
 		);
 	},
+	orderSushi: function (ing1, ing2, ing3) {
+		console.log(`You have order sushi with ${ing1}, ${ing2}, ${ing3}`);
+	},
 };
 
-japaneseRestaurant.foodDelivery({
-	deliveryTime: "12:30",
-	adress: "10 Markham Woods Rd",
-	mainMenuIndex: 0,
-	appetizersIndex: 1,
-});
+/************************************************************************************************************************************************* */
 
-japaneseRestaurant.foodDelivery({
-	adress: "10 Markham Woods Rd",
-});
+// // Spread operator
+
+// // Old bad approach
+// const arr = [1, 3, 5];
+// const newArr = [7, 9, arr[0], arr[1], arr[2]];
+// console.log(newArr);
+
+// // New approach with spread operator
+// const newArrSpread = [7, 9, ...arr];
+// console.log(newArrSpread);
+// console.log(...newArrSpread);
+
+// const newMenu = [...japaneseRestaurant.mainMenu, "Miso Salmon"];
+// console.log(newMenu);
+
+// // Array copying
+// const categoriesCopy = [...japaneseRestaurant.categories];
+
+// // Merge arrays
+// const bothMenu = [
+// 	...japaneseRestaurant.appetizers,
+// 	...japaneseRestaurant.mainMenu,
+// ];
+// console.log(bothMenu);
+
+// // Iterable - arrays, strings, maps, sets. OBJECTS AREN'T ITERABLE.
+
+// // Spread operator with strings
+// const greeting = "Hey";
+// const greetingLetters = [...greeting, " ", "!"];
+// console.log(greetingLetters);
+// console.log(...greetingLetters);
+// // console.log(`${...greeting}`); // Can't do this!!!
+
+// const ingridients = [
+// 	prompt(`Ingridient 1 for your sushi?`),
+// 	prompt(`Ingridient 2 for your sushi?`),
+// 	prompt(`Ingridient 3 for your sushi?`),
+// ];
+// console.log(ingridients);
+
+// japaneseRestaurant.orderSushi(...ingridients);
+
+// Objects
+const newJapaneseRestaurant = {
+	foundationDate: 2011,
+	...japaneseRestaurant,
+	owner: "Suzuki",
+};
+console.log(newJapaneseRestaurant);
+
+const japaneseRestaurantCopy = { ...japaneseRestaurant };
+japaneseRestaurantCopy.name = "Suzuki Sushi";
+console.log(japaneseRestaurant.name);
+console.log(japaneseRestaurantCopy.name);
 
 /************************************************************************************************************************************************* */
 
 // // Destructuring Objects
+
+// japaneseRestaurant.foodDelivery({
+// 	deliveryTime: "12:30",
+// 	adress: "10 Markham Woods Rd",
+// 	mainMenuIndex: 0,
+// 	appetizersIndex: 1,
+// });
+
+// japaneseRestaurant.foodDelivery({
+// 	adress: "10 Markham Woods Rd",
+// });
+
 // const { workingHours: hours, name: resName, categories } = japaneseRestaurant;
 // console.log(hours, resName, categories);
 
@@ -73,6 +135,7 @@ japaneseRestaurant.foodDelivery({
 /************************************************************************************************************************************************* */
 
 // // Destructuring Arrays
+
 // const arr = [3, 5, 7];
 // const x1 = arr[0];
 // const x2 = arr[1];
@@ -116,9 +179,5 @@ japaneseRestaurant.foodDelivery({
 
 // const [s = 0, i = 0, g = 0] = unknownArr;
 // console.log(s, i, g);
-
-/************************************************************************************************************************************************* */
-
-/************************************************************************************************************************************************* */
 
 /************************************************************************************************************************************************* */
