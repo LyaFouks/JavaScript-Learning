a879bf9ebffdfa4849e5e38bf2b71c052cc945ef
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-13 20:39:59 +0200

    Default options

---
+++ More about Functions/moreAboutFunctions.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/************************************************************************* */
+
+// DEFAULT OPTIONS
+
+const bookings = [];
+
+const makeBooking = function (
+	flightNum,
+	passengersNum = 1,
+	price = 99 * passengersNum
+) {
+	// Before ES6
+	// passengersNum = passengersNum || 1;
+	// price = price || 99;
+	const booking = {
+		flightNum,
+		passengersNum,
+		price,
+	};
+	console.log(booking);
+	bookings.push(booking);
+};
+
+makeBooking("QE123");
+makeBooking("TU235", 3, 390);
+makeBooking("TU235", 1);
+makeBooking("TU235", 4);
+makeBooking("TU215", undefined, 400);
+
+/************************************************************************* */
