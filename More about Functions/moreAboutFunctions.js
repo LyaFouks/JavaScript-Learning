adb89ae67ad7d97e266d9d62451cb9ab19030dc2
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-15 07:24:51 +0200

    Functions accepting callback functions

--- More about Functions/moreAboutFunctions.js
+++ More about Functions/moreAboutFunctions.js
@@ -5,44 +5,75 @@
 /************************************************************************* */
 /************************************************************************* */
 /************************************************************************* */
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+
+const removeSpaces = function (text) {
+	return text.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (text) {
+	const [first, ...others] = text.split(" ");
+	return [first.toUpperCase(), ...others].join(" ");
+};
+
+// High-Order function
+const converter = function (str, func) {
+	console.log(`The original text: ${str}`);
+	console.log(`The converted text: ${func(str)}`);
+
+	console.log(`Converted by: ${func.name}`);
+};
+
+converter("Hello to everyone!", upperFirstWord);
+converter("Hello to everyone!", removeSpaces);
+
+// Callback functions are very common in JS
+const twice = (num) => console.log(num * 2);
+
+[1, 2, 3].forEach(twice);
+
 /************************************************************************* */
+
+// FIRST CLASS FUNCTIONS and HIGH ORDER FUNCTIONS
+
 /************************************************************************* */
 
-// Passing arguments. Values and References
+// // PASSING AURGUMENST. VALUES AND REFERENCES
 
-const flightNumber = "BV234";
-const passenger1145 = {
-	firstName: "Jack",
-	lastName: "Brown",
-	passport: 128490,
-};
+// const flightNumber = "BV234";
+// const passenger1145 = {
+// 	firstName: "Jack",
+// 	lastName: "Brown",
+// 	passport: 128490,
+// };
 
-const checkIn = function (flight, passenger) {
-	flight = "bv234";
-	passenger.firstName = passenger.firstName.toLowerCase();
-	passenger.lastName = passenger.lastName.toLowerCase();
+// const checkIn = function (flight, passenger) {
+// 	flight = "bv234";
+// 	passenger.firstName = passenger.firstName.toLowerCase();
+// 	passenger.lastName = passenger.lastName.toLowerCase();
 
-	if (passenger.passport === 128490) {
-		alert("Welcome to the board!");
-	} else {
-		alert("Incorect passport!");
-	}
-};
+// 	if (passenger.passport === 128490) {
+// 		alert("Welcome to the board!");
+// 	} else {
+// 		alert("Incorect passport!");
+// 	}
+// };
 
-checkIn(flightNumber, passenger1145);
-console.log(flightNumber);
-console.log(passenger1145);
+// checkIn(flightNumber, passenger1145);
+// console.log(flightNumber);
+// console.log(passenger1145);
 
-// This is the same as doing it
-const flight = flightNumber;
-const passenger = passenger1145;
+// // This is the same as doing it
+// const flight = flightNumber;
+// const passenger = passenger1145;
 
-const changePassportNumber = function (person) {
-	person.passport = Math.trunc(Math.random() * 1000000);
-};
+// const changePassportNumber = function (person) {
+// 	person.passport = Math.trunc(Math.random() * 1000000);
+// };
 
-changePassportNumber(passenger1145);
-checkIn(flightNumber, passenger1145);
+// changePassportNumber(passenger1145);
+// checkIn(flightNumber, passenger1145);
 
 /************************************************************************* */
 
