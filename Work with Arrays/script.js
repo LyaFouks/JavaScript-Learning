4be35cb918686be6b7e7ccff85a5d3c745da4049
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-21 10:53:41 +0200

    some() and every() Methods

--- Work with Arrays/script.js
+++ Work with Arrays/script.js
@@ -11,60 +11,80 @@
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
+
+// // some() and every() METHODS
+
+// const transactions = [300, 250, -500, 5000, -750, -100, 50, 1400, -350, -120];
+
+// // console.log(transactions.includes(50));
+// // console.log(transactions.includes(150));
+
+// // some()
+// const hasWithdrawals = transactions.some((trans) => trans < 0);
+// console.log(hasWithdrawals);
+// const hasWithdrawalsMore5000 = transactions.some((trans) => trans < -5000);
+// console.log(hasWithdrawalsMore5000);
+
+// console.log(transactions.some((trans) => trans === 50));
+
+// // every()
+// console.log(transactions.every((trans) => trans < 0));
+// console.log(transactions.every((trans) => Math.abs(trans) > 49));
+
 //////////////////////////////////////////////////////////////////////////
 
-// find() METHOD  // Does not return a new array. Returns the first matching value
+// // find() METHOD  // Does not return a new array. Returns the first matching value
 
-const transactions = [300, 250, -500, 5000, -750, -100, 50, 1400, -350, -120];
+// const transactions = [300, 250, -500, 5000, -750, -100, 50, 1400, -350, -120];
 
-const firstDeposit = transactions.find((trans) => trans > 0);
-const firstWithdrawal = transactions.find((trans) => trans < 0);
-console.log(firstDeposit);
-console.log(firstWithdrawal);
+// const firstDeposit = transactions.find((trans) => trans > 0);
+// const firstWithdrawal = transactions.find((trans) => trans < 0);
+// console.log(firstDeposit);
+// console.log(firstWithdrawal);
 
-// Second example
-const account1 = {
-	userName: "Cecil Ireland",
-	transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
-	interest: 1.5,
-	pin: 1111,
-};
+// // Second example
+// const account1 = {
+// 	userName: "Cecil Ireland",
+// 	transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+// 	interest: 1.5,
+// 	pin: 1111,
+// };
 
-const account2 = {
-	userName: "Amani Salt",
-	transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
-	interest: 1.3,
-	pin: 2222,
-};
+// const account2 = {
+// 	userName: "Amani Salt",
+// 	transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+// 	interest: 1.3,
+// 	pin: 2222,
+// };
 
-const account3 = {
-	userName: "Corey Martinez",
-	transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
-	interest: 0.8,
-	pin: 3333,
-};
+// const account3 = {
+// 	userName: "Corey Martinez",
+// 	transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+// 	interest: 0.8,
+// 	pin: 3333,
+// };
 
-const account4 = {
-	userName: "Kamile Searle",
-	transactions: [530, 1300, 500, 40, 190],
-	interest: 1,
-	pin: 4444,
-};
+// const account4 = {
+// 	userName: "Kamile Searle",
+// 	transactions: [530, 1300, 500, 40, 190],
+// 	interest: 1,
+// 	pin: 4444,
+// };
 
-const account5 = {
-	userName: "Oliver Avila",
-	transactions: [630, 800, 300, 50, 120],
-	interest: 1.1,
-	pin: 5555,
-};
+// const account5 = {
+// 	userName: "Oliver Avila",
+// 	transactions: [630, 800, 300, 50, 120],
+// 	interest: 1.1,
+// 	pin: 5555,
+// };
 
-const accounts = [account1, account2, account3, account4, account5];
+// const accounts = [account1, account2, account3, account4, account5];
 
-const coreyMartinezAccount = accounts.find(
-	(account) => account.userName === "Corey Martinez"
-);
+// const coreyMartinezAccount = accounts.find(
+// 	(account) => account.userName === "Corey Martinez"
+// );
 
-console.log(coreyMartinezAccount);
+// console.log(coreyMartinezAccount);
 
 //////////////////////////////////////////////////////////////////////////
 
