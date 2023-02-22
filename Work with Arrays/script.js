691c9c7b50a4e7de486df81b9551db6d8275b775
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-02-22 09:25:55 +0200

    Array Methods in Practice

--- Work with Arrays/script.js
+++ Work with Arrays/script.js
@@ -7,34 +7,137 @@
 // ]);
 
 // const transactions = [300, 250, -500, 5000, -750, -100, 50, 1400, -350, -120];
+
 //////////////////////////////////////////////////////////////////////////
 
-// OTHER WAYS TO CREATE AND POPULATE ARRAYS
+// // Array Methods in Practice
 
-// Empty Arrays
-console.log([1, 2, 3]);
-console.log(new Array(1, 2, 3));
+// const account1 = {
+// 	userName: "Cecil Ireland",
+// 	transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+// 	interest: 1.5,
+// 	pin: 1111,
+// };
 
-const arr = new Array(3);
-console.log(arr);
-// console.log(arr.map(() => 7));
+// const account2 = {
+// 	userName: "Amani Salt",
+// 	transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+// 	interest: 1.3,
+// 	pin: 2222,
+// };
 
-// fill() Method
-// arr.fill(7);
-// arr.fill(7, 1);
-arr.fill(7, 1, 2);
-console.log(arr);
+// const account3 = {
+// 	userName: "Corey Martinez",
+// 	transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+// 	interest: 0.8,
+// 	pin: 3333,
+// };
 
-const arr1 = [1, 1, 1, 1, 1];
-arr1.fill(3, 2, 4);
-console.log(arr1);
+// const account4 = {
+// 	userName: "Kamile Searle",
+// 	transactions: [530, 1300, 500, 40, 190],
+// 	interest: 1,
+// 	pin: 4444,
+// };
 
-// Array.from() Method
-const arr2 = Array.from({ length: 5 }, () => 1);
-console.log(arr2);
+// const account5 = {
+// 	userName: "Oliver Avila",
+// 	transactions: [630, 800, 300, 50, 120],
+// 	interest: 1.1,
+// 	pin: 5555,
+// };
 
-const arr3 = Array.from({ length: 5 }, (_, index) => index + 1);
-console.log(arr3);
+// const accounts = [account1, account2, account3, account4, account5];
+
+// // Ex. 1
+// const bankDepositTotal = accounts
+// 	.flatMap((account) => account.transactions)
+// 	.filter((trans) => trans > 0)
+// 	.reduce((acc, depos) => acc + depos, 0);
+// console.log(bankDepositTotal);
+
+// // Ex. 2
+// const withdrawalsOver300 = accounts
+// 	.flatMap((account) => account.transactions)
+// 	.filter((trans) => trans <= -300).length;
+// console.log(withdrawalsOver300);
+
+// const withdrawalsOver300Num1 = accounts
+// 	.flatMap((account) => account.transactions)
+// 	.reduce((counter, trans) => (trans <= -300 ? counter + 1 : counter), 0);
+// console.log(withdrawalsOver300Num1);
+
+// // Ex. 3
+// const { depositsTotal, withdrawalsTotal } = accounts
+// 	.flatMap((account) => account.transactions)
+// 	.reduce(
+// 		(acc, trans) => {
+// 			// trans > 0
+// 			// 	? (acc.depositsTotal += trans)
+// 			// 	: (acc.withdrawalsTotal += trans);
+// 			acc[trans > 0 ? "depositsTotal" : "withdrawalsTotal"] += trans;
+// 			return acc;
+// 		},
+// 		{ depositsTotal: 0, withdrawalsTotal: 0 }
+// 	);
+
+// console.log(depositsTotal, withdrawalsTotal);
+
+// // Ex. 4
+// // work with arrays in javascript -> Work with Arrays in Javascript
+
+// const text1 = "work with arrays in javascript";
+// const text2 =
+// 	"work with arrays in javascript with simple language for beginners";
+// const text3 = "work with arrays and strings in javascript";
+// const text4 = "for what need arrays in javascript?";
+
+// const textTotTitleCase = function (text) {
+// 	const exeptions = ["with", "in", "and", "for", "a", "an", "at", "on"];
+
+// 	const capitalizeString = (word) => word[0].toUpperCase() + word.slice(1);
+
+// 	const titleCase = text
+// 		.toLowerCase()
+// 		.split(" ")
+// 		.map((word) => (exeptions.includes(word) ? word : capitalizeString(word)))
+// 		.join(" ");
+// 	return capitalizeString(titleCase);
+// };
+
+// console.log(textTotTitleCase(text1));
+// console.log(textTotTitleCase(text2));
+// console.log(textTotTitleCase(text3));
+// console.log(textTotTitleCase(text4));
+
+//////////////////////////////////////////////////////////////////////////
+
+// // OTHER WAYS TO CREATE AND POPULATE ARRAYS (fill() and Array.from() Methods)
+
+// // Empty Arrays
+// console.log([1, 2, 3]);
+// console.log(new Array(1, 2, 3));
+
+// const arr = new Array(3);
+// console.log(arr);
+// // console.log(arr.map(() => 7));
+
+// // fill() Method
+// // arr.fill(7);
+// // arr.fill(7, 1);
+// arr.fill(7, 1, 2);
+// console.log(arr);
+
+// const arr1 = [1, 1, 1, 1, 1];
+// arr1.fill(3, 2, 4);
+// console.log(arr1);
+
+// // Array.from() Method
+// const arr2 = Array.from({ length: 5 }, () => 1);
+// console.log(arr2);
+
+// const arr3 = Array.from({ length: 5 }, (_, index) => index + 1);
+// console.log(arr3);
 
 //////////////////////////////////////////////////////////////////////////
 
