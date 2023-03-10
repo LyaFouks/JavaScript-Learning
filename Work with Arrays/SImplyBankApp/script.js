6246bee8bb899ca3a62ee7ce1d45c33ac99174ee
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-03-10 07:51:11 +0200

    Operations with Dates in SBA

--- Work with Arrays/SImplyBankApp/script.js
+++ Work with Arrays/SImplyBankApp/script.js
@@ -14,8 +14,8 @@ const account1 = {
 		"2021-01-22T12:17:46.255Z",
 		"2021-02-12T15:14:06.486Z",
 		"2021-03-09T11:42:26.371Z",
-		"2021-05-21T07:43:59.331Z",
-		"2021-06-22T15:21:20.814Z",
+		"2023-03-05T07:43:59.331Z",
+		"2023-03-09T15:21:20.814Z",
 	],
 	currency: "USD",
 	locale: "en-US",
@@ -121,6 +121,28 @@ const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////////////////////////////
 
+// Display Date
+
+const formatTransactionDate = function (date) {
+	const getDaysBetween2Dates = (date1, date2) =>
+		Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+	const daysPassed = getDaysBetween2Dates(new Date(), date);
+	console.log(daysPassed);
+
+	if (daysPassed === 0) return "Today";
+	if (daysPassed === 1) return "Yesterday";
+	if (daysPassed <= 7) return `${daysPassed} days ago`;
+	else {
+		const day = `${date.getDate()}`.padStart(2, "0");
+		const month = `${date.getMonth() + 1}`.padStart(2, "0");
+		const year = date.getFullYear();
+
+		return `${day}/${month}/${year}`;
+	}
+};
+///////////////////////////////////////////////////////////////////////////
+
 // Creating DOM elements
 
 const displayTransactions = function (account, sort = false) {
@@ -135,11 +157,7 @@ const displayTransactions = function (account, sort = false) {
 
 		const date = new Date(account.transactionsDates[index]);
 
-		const day = `${date.getDate()}`.padStart(2, "0");
-		const month = `${date.getMonth() + 1}`.padStart(2, "0");
-		const year = date.getFullYear();
-
-		const transDate = `${day}/${month}/${year}`;
+		const transDate = formatTransactionDate(date);
 
 		const transactionRow = `
     <div class="transactions__row">
