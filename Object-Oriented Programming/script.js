cd8e1a0167dd65fef28a8a92c907220335f8035d
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2023-04-04 07:56:38 +0300

    ES6 Classes

--- Object-Oriented Programming/script.js
+++ Object-Oriented Programming/script.js
@@ -2,95 +2,137 @@
 
 ///////////////////////////////////////////////////////////////////////////
 
-// Functions-constructor and Operator new
+// // Functions-constructor and Operator new
 
-const Person = function (firstName, birthYear) {
-	// Instance properties - свойство экземпляра
-	this.firstName = firstName;
-	this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+// 	// Instance properties - свойство экземпляра
+// 	this.firstName = firstName;
+// 	this.birthYear = birthYear;
 
-	// // Never do it!!!
-	// this.printAge = function () {
-	// 	console.log(2023 - this.birthYear);
-	// };
-};
+// 	// // Never do it!!!
+// 	// this.printAge = function () {
+// 	// 	console.log(2023 - this.birthYear);
+// 	// };
+// };
 
-const nikita = new Person("Nikita", 1991);
-console.log(nikita);
+// const nikita = new Person("Nikita", 1991);
+// console.log(nikita);
 
-// 1. Creating new empty object {}
-// 2. Function is called, this = {}
-// 3. {} connects with prototype
-// 4. The function automatically returns {}
+// // 1. Creating new empty object {}
+// // 2. Function is called, this = {}
+// // 3. {} connects with prototype
+// // 4. The function automatically returns {}
 
-const mary = new Person("Mary", 2000);
-const john = new Person("John", 1995);
-console.log(mary, john);
+// const mary = new Person("Mary", 2000);
+// const john = new Person("John", 1995);
+// console.log(mary, john);
 
-const jack = "Jack";
+// const jack = "Jack";
 
-console.log(mary instanceof Person);
-console.log(jack instanceof Person);
-
-///////////////////////////////////////////////////////////////////////////
-
-// Prototypes
-
-console.log(Person.prototype);
-
-Person.prototype.printAge = function () {
-	console.log(2023 - this.birthYear);
-};
-
-nikita.printAge();
-mary.printAge();
-john.printAge();
-
-console.log(nikita.__proto__);
-console.log(nikita.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(mary));
-console.log(Person.prototype.isPrototypeOf(john));
-console.log(Person.prototype.isPrototypeOf(Person));
-
-Person.prototype.creatureClass = "Mammal";
-console.log(mary, john);
-console.log(mary.hasOwnProperty("birthYear"));
-console.log(mary.hasOwnProperty("creatureClass"));
+// console.log(mary instanceof Person);
+// console.log(jack instanceof Person);
 
 ///////////////////////////////////////////////////////////////////////////
 
-// Prototype Inheritance for built-in Objectcs
+// // Prototypes
 
-// Person.prototype
-console.log(nikita.__proto__);
+// console.log(Person.prototype);
 
-// Object.prototype - верхушка цепи прототипов
-console.log(nikita.__proto__.__proto__);
+// Person.prototype.printAge = function () {
+// 	console.log(2023 - this.birthYear);
+// };
 
-// null
-console.log(nikita.__proto__.__proto__.__proto__);
+// nikita.printAge();
+// mary.printAge();
+// john.printAge();
 
-console.log(Person.prototype.constructor);
-console.dir(Person.prototype.constructor);
+// console.log(nikita.__proto__);
+// console.log(nikita.__proto__ === Person.prototype);
 
-const numbers = [3, 5, 7, 9, 4, 3, 7, 6, 7, 9];
-console.log(numbers.__proto__);
-console.log(Object.getPrototypeOf(numbers));
-console.log(numbers.__proto__ === Array.prototype);
-console.log(numbers.__proto__.__proto__);
+// console.log(Person.prototype.isPrototypeOf(mary));
+// console.log(Person.prototype.isPrototypeOf(john));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-Array.prototype.uniqueElements = function () {
-	return [...new Set(this)];
-};
+// Person.prototype.creatureClass = "Mammal";
+// console.log(mary, john);
+// console.log(mary.hasOwnProperty("birthYear"));
+// console.log(mary.hasOwnProperty("creatureClass"));
 
-console.log(numbers.uniqueElements());
+///////////////////////////////////////////////////////////////////////////
 
-const h2 = document.querySelector("h2");
-console.dir(h2);
+// // Prototype Inheritance for built-in Objectcs
 
-console.log((n) => n * 2);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Person.prototype
+// console.log(nikita.__proto__);
+
+// // Object.prototype - верхушка цепи прототипов
+// console.log(nikita.__proto__.__proto__);
+
+// // null
+// console.log(nikita.__proto__.__proto__.__proto__);
+
+// console.log(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
+
+// const numbers = [3, 5, 7, 9, 4, 3, 7, 6, 7, 9];
+// console.log(numbers.__proto__);
+// console.log(Object.getPrototypeOf(numbers));
+// console.log(numbers.__proto__ === Array.prototype);
+// console.log(numbers.__proto__.__proto__);
+
+// Array.prototype.uniqueElements = function () {
+// 	return [...new Set(this)];
+// };
+
+// console.log(numbers.uniqueElements());
+
+// const h2 = document.querySelector("h2");
+// console.dir(h2);
+
+// console.log((n) => n * 2);
+///////////////////////////////////////////////////////////////////////////
+
+// ES6 Classes
+
+// // Class expression
+// const Person = class {
+
+// }
+
+// Class declaration
+class Person {
+	constructor(firstName, birthYear) {
+		this.firstName = firstName;
+		this.birthYear = birthYear;
+	}
+
+	// Методы добавляются в свойство .prototype
+	printAge() {
+		console.log(2023 - this.birthYear);
+	}
+
+	greet() {
+		console.log(`Hello! My name is ${this.firstName}`);
+	}
+}
+
+const jack = new Person("Jack", 2000);
+console.log(jack);
+jack.printAge();
+console.log(jack.__proto__ === Person.prototype);
+console.log(Object.getPrototypeOf(jack) === Person.prototype);
+
+// Person.prototype.greet = function () {
+// 	console.log(`Hello! My name is ${this.firstName}`);
+// };
+
+jack.greet();
+
+// 1. Классы не поднимаются при помощи hoisting
+// 2. Классы являются "first class citizens"
+// 3. Тело класса выполняется в strict режиме
+
+///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
